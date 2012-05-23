860d16cf1eb12c3c47e6c4a55d5f3766a75e719b
Jonathan Wade <jnwade@fullsail.edu>
2012-05-23 18:30:19 -0400

    Update

--- library.js
+++ library.js
@@ -76,6 +76,24 @@ var jnwadeLib = function(){
 var lib = jnwadeLib();
 
 
+lib.compareDates(new Date(2012,4,20), new Date());
+
+
+lib.checkEmail("jnwade@fullsail.com");
+
+ 
+lib.addDecimal(5.1);
+
+
+lib.stringify([40, 25, 18, 16]);
+    
+
+lib.checkPhoneNumber("(407) 443-2792");
+
+
+lib.checkUrl("portal.online.fullsail.edu");
+
+
 
 
 
