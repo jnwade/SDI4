97bc4537a47f090d3f41c6ab1a91ba52abded493
Jonathan Wade <jnwade@fullsail.edu>
2012-05-24 23:21:57 -0400

    Finished

--- test.js
+++ test.js
@@ -1,17 +1,17 @@
-lib.compareDates(new Date(2012,4,20), new Date());
+lib.compareDates(new Date(2012,4,20), new Date()); // Testing date comparisons
 
 
-lib.checkEmail("jnwade@fullsail.com");
+lib.checkEmail("jnwade@fullsail.com"); // Validating an Email Address
 
  
-lib.addDecimal(5.1);
+lib.addDecimal(5.1); // Adding Decimal Places
 
 
-lib.stringify([100, 120, 97, 88]);
+lib.stringify([100, 120, 97, 88]); // Converting Number Values to a String
     
 
-lib.checkPhoneNumber("(407) 443-2792");
+lib.checkPhoneNumber("(407) 443-2792"); // Validating a Phone Nummber
 
 
-lib.checkUrl("portal.online.fullsail.edu");
+lib.checkUrl("portal.online.fullsail.edu"); // Validating a URL
 
